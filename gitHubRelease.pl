864ebd9d910f89f08deaf2a7589324260aa9e467
mohb apps <mohb.apps@gmail.com>
2017-01-07 00:00:56 -0200

    - Added option '-t' to gitHubRelease.pl script to upload tests directories - Now gitHubRelese.pl checks if the destination directory exists and create it if don't - Added comments to both scripts

--- gitHubRelease.pl
+++ gitHubRelease.pl
@@ -1,48 +1,78 @@
 #!/usr/bin/perl
 
-my $project;
-my $originPath;
-my $mdFilesPath;
-my $destinationPath;
-my $gradle_file; 
-my $commitMessage;
-my $mdFilesPath;
-my $destinationPath;
-my $gradle_file;
-my @gradle_file_lines;
+#
+#  File          : gitHubRelease.pl
+#  Last modified : 01/06/17 8:31 PM
+#
+#  Developer     : Haraldo Albergaria Filho, a.k.a. mohb apps
+#
+#  Description   : Script to upload code of releases to GitHub
+#  Usage         : gitHubRelease.pl <project> [-t]
+#
+#  --------------------------------------------------------------
 
-$project      = $ARGV[0];
-$include_test = $ARGV[1];
 
-$destinationPath = "/home/mohb/GitHubReleases/$project";
-$mdFilesPath = "/home/mohb/mdFiles/$project/";
+my $project          = $ARGV[0];
+my $include_test     = $ARGV[1];
 
-if($include_test =~ /-t/) {
-	$originPath = "/home/mohb/AndroidStudioProjects/$project/app/src/";
-	system "rsync -rutv --force --exclude='debug' --exclude='release' --exclude='drawable*' --exclude='mipmap*' $originPath $destinationPath";
-}    
-else {
-	$originPath = "/home/mohb/AndroidStudioProjects/$project/app/src/main/";
-	system "rsync -rutv --force --exclude='drawable*' --exclude='mipmap*' $originPath $destinationPath";
+my $home_dir         = $ENV{'HOME'};
+
+my $destinationPath  = $home_dir."/GitHubReleases/$project";
+my $mdFilesPath      = $home_dir."/mdFiles/$project/";
+
+my $gradle_file      = $home_dir."/AndroidStudioProjects/".$project."/app/build.gradle";
+
+
+###### COPY PROJECT FILES TO LOCAL GIT REPOSITORY ########################################################
+
+# Check if destination directory already exists
+if (not -e $destinationPath) {
+	# Create destination directory
+	!system "mkdir $destinationPath" or die "\nCan't create directory $destinationPath\n\n";
 }
 
-system "rsync -rutv --force $mdFilesPath $destinationPath";
+# If there is no second argument, copy only main application source files
+if(!$include_test) {
+	$originPath = $home_dir."/AndroidStudioProjects/$project/app/src/main/";
+	system "rsync -rutv --force --exclude='drawable*' --exclude='mipmap*' $originPath $destinationPath";
+}    
+else {
+	# If second argument is "-t", copy all source files, including tests 
+	if($include_test =~ /-t/) {
+		$originPath = $home_dir."/AndroidStudioProjects/$project/app/src/";
+		system "rsync -rutv --force --exclude='debug' --exclude='release' --exclude='drawable*' --exclude='mipmap*' $originPath $destinationPath";
+	}
+	else { # display message of invalid option
+		die "Invalid option: $include_test\n";
+	}
+}
 
-$gradle_file = "/home/mohb/AndroidStudioProjects/".$project."/app/build.gradle";
+# Check if there are markdown files
+if(-s $mdFilesPath) {
+	# Copy markdown files
+	system "rsync -rutv --force $mdFilesPath $destinationPath";
+}
 
+###### GET VERSION NAME FROM GRADLE BUILD FILE ############################################################
+
+# Read file
 open GRADLE, "$gradle_file" or die "Can't open $gradle_file to read: $!\n";
-@gradle_file_lines = <GRADLE>;
+my @gradle_file_lines = <GRADLE>;
 close GRADLE;
 
+# Search for versionName and set it as the commit message
+my $commitMessage;
 foreach(@gradle_file_lines) {
 	$_ =~ /versionName\s\"(.+)\"/;
 	$commitMessage = $1;
 }
 
-chdir $destinationPath;
-system "git add *";
-system "git commit -m $commitMessage";
-system "git push -u origin master";
 
+###### COMMIT FILES AND PUSH THEM TO REMOTE REPOSITORY ####################################################
+
+#chdir $destinationPath;
+#system "git add *";
+#system "git commit -m $commitMessage";
+#system "git push -u origin master";
 
 
--- updateProjectVersion.pl
+++ updateProjectVersion.pl
@@ -1,44 +1,74 @@
 #!/usr/bin/perl
 
-my $project      = $ARGV[0];
-my $versionCode  = $ARGV[1];
-my $versionName  = $ARGV[2];
+#
+#  File          : updateProjectVersion.pl
+#  Last modified : 01/06/17 11:31 PM
+#
+#  Developer     : Haraldo Albergaria Filho, a.k.a. mohb apps
+#
+#  Description   : Script to update app release versions
+#  Usage         : updateProjectVersion.pl <project> <versionCode> <versionName>
+#
+#  ---------------------------------------------------------------------------------
 
-my $gradle_file  = "/home/mohb/AndroidStudioProjects/".$project."/app/build.gradle";
-my $strings_file = "/home/mohb/AndroidStudioProjects/".$project."/app/src/main/res/values/strings.xml";
+my $project       = $ARGV[0];
+my $versionCode   = $ARGV[1];
+my $versionName   = $ARGV[2];
 
+my $home_dir      = $ENV{'HOME'};
+
+my $gradle_file   = $home_dir."/AndroidStudioProjects/".$project."/app/build.gradle";
+my $strings_file  = $home_dir."/AndroidStudioProjects/".$project."/app/src/main/res/values/strings.xml";
+
+
+###### READ DATA FROM FILES ########################################################################################
+
+# Open files for reading
 open GRADLE, "$gradle_file" or die "Can't open $gradle_file to read: $!\n";
 open STRINGS, "$strings_file" or die "Can't open $strings_file to read: $!\n";
 
+# Read files
 my @gradle_file_lines = <GRADLE>;
 my @strings_file_lines = <STRINGS>;
 
+# Close files
 close GRADLE;
 close STRINGS;
 
+
+###### UPDATE DATA #################################################################################################
+
+# Search for and update versionCode and versionName on build.gradle
 foreach(@gradle_file_lines) {
 	$_ =~ s/versionCode\s(\d+)/versionCode $versionCode/;
 	$_ =~ s/versionName\s\"(.+)\"/versionName "$versionName"/;
 }
 
+# Search for and update versionCode and versionName on strings.xml
 foreach(@strings_file_lines) {
 	$_ =~ s/(\<string\sname=\"version_number\"\stranslatable=\"false\"\>)(.+)(\<\/string\>)/$1$versionName$3/;
 	$_ =~ s/(\<string\sname=\"version_code\"\stranslatable=\"false\"\>)(.+)(\<\/string\>)/$1$versionCode$3/;
 }
 
+
+###### WRITE DATA TO FILES #########################################################################################
+
+# Open files for writing
 open GRADLE, ">$gradle_file" or die "Can't open $gradle_file to write: $!\n";
 open STRINGS, ">$strings_file" or die "Can't open $strings_file to write: $!\n";
 
+# Write updated data to build.gradle
 foreach(@gradle_file_lines) {
 	print GRADLE $_;
 }
 
+# Write updated data to strings.xml
 foreach(@strings_file_lines) {
 	print STRINGS $_;
 }
 
+# Close files
 close GRADLE;
 close STRINGS;
-
 
 
